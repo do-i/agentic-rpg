7988d61b46dc8d97574e62ac5b8217cffc038487
Ninja <ninja@renton.net>
2026-03-15 20:47:04 -0400

    Add scene and scene_manager classes

--- engine/core/container.py
+++ engine/core/container.py
@@ -3,14 +3,17 @@
 from dependency_injector import containers, providers
 from engine.core.display import Display
 from engine.core.frame_clock import FrameClock
+from engine.core.scene_manager import SceneManager
 from engine.core.game import Game
 
 
 class Container(containers.DeclarativeContainer):
     display = providers.Singleton(Display)
     clock = providers.Singleton(FrameClock)
+    scene_manager = providers.Singleton(SceneManager)
     game = providers.Singleton(
         Game,
         display=display,
         clock=clock,
+        scene_manager=scene_manager,
     )
--- engine/core/game.py
+++ engine/core/game.py
@@ -3,31 +3,29 @@
 import pygame
 from engine.core.display import Display
 from engine.core.frame_clock import FrameClock
+from engine.core.scene_manager import SceneManager
 
 
 class Game:
-    def __init__(self, display: Display, clock: FrameClock) -> None:
+    def __init__(self, display: Display, clock: FrameClock, scene_manager: SceneManager) -> None:
         self._display = display
         self._clock = clock
+        self._scene_manager = scene_manager
         self._running = False
 
     def run(self) -> None:
         self._running = True
         while self._running:
             self._clock.tick()
-            self._handle_events()
-            self._update()
-            self._render()
+            events = pygame.event.get()
+            self._handle_events(events)
+            self._scene_manager.update(self._clock.delta)
+            self._scene_manager.render(self._display.screen)
             self._display.flip()
         self._display.quit()
 
-    def _handle_events(self) -> None:
-        for event in pygame.event.get():
+    def _handle_events(self, events: list) -> None:
+        for event in events:
             if event.type == pygame.QUIT:
                 self._running = False
-
-    def _update(self) -> None:
-        pass
-
-    def _render(self) -> None:
-        self._display.screen.fill((0, 0, 0))
+        self._scene_manager.handle_events(events)
